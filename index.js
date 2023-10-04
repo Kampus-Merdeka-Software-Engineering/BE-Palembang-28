2d622d6b2abd0be5380ad3d6873dce0977c45ef1
Picessa Kresna <kresna.picessa@gmail.com>
2023-10-04 22:01:27 +0800

    test railway

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import express from "express";
 import cors from "cors";
 import keluhanRoute from "./routes/keluhanRoute.js";
 
+const port = process.env.PORT || 3000;
+
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(keluhanRoute);
 
-app.listen(3000, () => console.log("Server up and running..."));
+app.listen(port, "0.0.0.0", function () {
+  console.log("Server up and running...");
+});

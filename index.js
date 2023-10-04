96b41c4346a8ea76f51f52f816500b5e225ee5d2
Picessa Kresna <kresna.picessa@gmail.com>
2023-10-04 22:05:15 +0800

    ubah

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import keluhanRoute from "./routes/keluhanRoute.js";
 
 const port = process.env.PORT || 3000;
 
-const bodyParser = require("body-parser");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(keluhanRoute);
 
 app.listen(port, "0.0.0.0", function () {
